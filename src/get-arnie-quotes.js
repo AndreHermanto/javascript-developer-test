a4bc43c70102cfbde39055a4fd1b2b5520674c49
andre <andre.hermanto93@gmail.com>
2022-02-17 18:37:17 +1100

    Coding test solution

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,22 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const result = Promise.all(
+    urls.map(async (url) => {
+      try{
+        const data = await httpGet(url)
+        const res = JSON.parse(data.body)
+        if(data.status !== 200) 
+          return {'FAILURE': res.message }
+        return {
+          ["Arnie Quote"]: res.message
+        }
+      }catch(e) {
+        return e;
+      }
+    })
+  )
+  return result;
 };
 
 module.exports = {
